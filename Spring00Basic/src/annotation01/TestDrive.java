603bee4e5dddd8951c1c85b995c300b37a681d32
KOITT <KOITT@KOITT-PC17>
2018-02-07 17:32:34 +0900

    Spring - 애노테이션

---
+++ Spring00Basic/src/annotation01/TestDrive.java
@@ -0,0 +1,21 @@
+package annotation01;
+
+public class TestDrive {
+	
+	public static void main(String[] args) {
+		
+		UserA userA = new UserA("S001", "kim");
+		UserB userB = new UserB("1", "Hong");
+		
+		IdExtractor idExtractor = new IdExtractor();
+		
+		String idA = idExtractor.extractId(userA);
+		String idB = idExtractor.extractId(userB);
+		
+		System.out.println("UserA의 id 값: " + idA);
+		System.out.println("UserB의 id 값: " + idB);
+		
+		
+	}
+
+}
